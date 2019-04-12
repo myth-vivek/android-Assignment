edc28e66707f283330227bd5d626d9fc3e0cc57d
vivek Magar <vivekmgr229@gmail.com>
2019-04-12 23:22:01 +0545

    first commit

---
+++ app/src/main/java/com/example/question1/dataModel.java
@@ -0,0 +1,28 @@
+package com.example.question1;
+
+public class dataModel {
+
+        private Float weight,height;
+
+        public dataModel(Float weight, Float height) {
+            this.weight = weight;
+            this.height = height;
+        }
+
+        public Float getWeight() {
+            return weight;
+        }
+
+        public void setWeight(Float weight) {
+            this.weight = weight;
+        }
+
+        public Float getHeight() {
+            return height;
+        }
+
+        public void setHeight(Float height) {
+            this.height = height;
+        }
+}
+
